a8d55502058707d6b99102e1c1670df677196f53
pattycha <pattycha@student.21-school.ru>
2023-05-30 00:49:03 +0700

    123

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+
+HOST = '51.250.65.169'  # The server's hostname or IP address
+PORT = 5002
+s = socket.socket()
+s.connect((HOST, PORT))
+print(f"[+] Connected")
+name = input("Enter your name: ")
+def listen():
+    while True:
+        data = s.recv(1024)
+        print("\n"+data.decode('utf-8'))
+t = threading.Thread(target=listen)
+t.daemon = True
+t.start()
+while True:
+    msg = input("")
+    if msg == "exit":
+        break
+    msg = name + ": " + msg
+    s.send(msg.encode('utf-8'))
+s.close()
