aef36ddf49285d77a1e8bab1b432a90f2cb03035
eComS <eComS@live.com>
2023-07-08 19:55:10 +0700

    123

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'advertisements.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- advertisements/app_adv/views.py
+++ advertisements/app_adv/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
-    return HttpResponse("ALL GOOD!")
+    return render(request, 'index.html')
+
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
