8eb7533e8cf4a5b055ed023d20f7a8c99b5d151b
eComS <eComS@live.com>
2023-07-08 21:26:09 +0700

    123

--- advertisements/app_adv/views.py
+++ advertisements/app_adv/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("ALL GOOD!")
+    return render(request, 'index.html')
