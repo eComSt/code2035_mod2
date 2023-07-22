a556e2c7de46da2a46278b99e16ab3a3ac132b87
eComS <eComS@live.com>
2023-07-22 21:51:13 +0700

    123

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField("Заголовок",max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=20, decimal_places=2)
+    auction = models.BooleanField("Торг", default=False,help_text="Отметьте если торг уместен")
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField("Дата обновления", auto_now=True)
+    def __str__(self):
+        return self.title
---
+++ emp.py
@@ -0,0 +1,5 @@
+class Employee:
+    name:str
+    position:str
+    qualification:str
+    salary:int
