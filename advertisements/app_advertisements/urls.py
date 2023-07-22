1de328a17a470af8fe630dfa91f86c5f31ef5829
eComS <eComS@live.com>
2023-07-22 20:23:55 +0700

    123

---
+++ advertisements/app_advertisements/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import index, top_sellers
+urlpatterns = [
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+]
---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
