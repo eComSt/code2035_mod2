7921ccb50b70d5ee9fff613c2725f35acfea7fab
eComS <eComS@live.com>
2023-08-05 20:26:33 +0700

    234

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -19,13 +19,12 @@ class Advertisement(models.Model):
             return format_html('<span style="color:green; font-weight: bold">СЕГОДНЯ В {}</span>', created_time)
 
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
-    @admin.display(description="дата создания")
+    @admin.display(description="дата обновления")
     def updated_date(self):
         from django.utils import timezone
         if self.updated_at.date()==timezone.now().date():
             created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html('<span style="color:red; font-weight: bold">СЕГОДНЯ В {}</span>', created_time)
-
         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
