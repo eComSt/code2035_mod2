f45fcc60b33628eacad26dc11c193108125840ca
eComS <eComS@live.com>
2023-08-05 20:23:58 +0700

    123

--- advertisements/advertisements/urls.py
+++ advertisements/advertisements/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('adm/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',include("app_advertisements.urls"))
 ]
